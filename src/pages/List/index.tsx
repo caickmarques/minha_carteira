5054fb0c8fc4dbc3cab43a48ea1daca1dbc03d4a
caickmarques <caick_marques@hotmail.com>
2021-02-25 17:00:07 -0300

    Finalizado layout das listas

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,20 +1,42 @@
 import React from 'react';
 import ContentHeader from '../../components/ContentHeader';
-import { Container } from './styles';
+import { Container, Content, Filters } from './styles';
 import SelectInput from '../../components/SelectInput';
+import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 
 const List: React.FC = () => {
-    const options = [
-        { value: 'Caick', label: 'Caick' },
-        { value: 'Maria', label: 'Maria' },
-        { value: 'Ana', label: 'Ana' },
+    const months = [
+        { value: 7, label: 'Julho' },
+        { value: 8, label: 'Agosto' },
+        { value: 9, label: 'Setembro' },
+    ];
+
+    const years = [
+        { value: 2020, label: 2020 },
+        { value: 2019, label: 2019 },
+        { value: 2018, label: 2018 },
     ];
 
     return (
         <Container>
             <ContentHeader title='Saídas' lineColor='#E44C4E'>
-                <SelectInput options={options} />
+                <SelectInput options={months} />
+                <SelectInput options={years} />
             </ContentHeader>
+
+            <Filters>
+                <button type='button' className='tag-filter recurrent'>Recorrentes</button>
+                <button type='button' className='tag-filter eventual'>Eventuais</button>
+            </Filters>
+
+            <Content>
+                <HistoryFinanceCard
+                    tagColor='#E44C4E'
+                    title='Conta de luz'
+                    subtitle='25/02/2021'
+                    amount='R$ 120,00'
+                />
+            </Content>
         </Container>
     );
 }
