7be20887435162f4f9e82d20bfb62b7f0e7ac78a
caickmarques <caick_marques@hotmail.com>
2021-03-11 15:30:49 -0300

    Adicionado efeitos de entrada nos componentes

--- src/components/HistoryBox/styles.ts
+++ src/components/HistoryBox/styles.ts
@@ -1,8 +1,23 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 interface ILegendProps {
     color: string;
-}
+};
+
+const animate = keyframes`
+    0%{
+        transform: translateX(-100px);
+        opacity: 0;
+    }
+    50%{
+        opacity: .3;
+    }
+
+    100%{
+        transform: translateX(0px);
+        opacity: 1
+    }
+`;
 
 export const Container = styled.div`
     width: 100%;
@@ -15,6 +30,7 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
+    animation: ${animate} .5s ease-in;
 `;
 
 export const Header = styled.header`
